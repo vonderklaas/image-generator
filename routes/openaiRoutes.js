d919b9b2ab2027fb77e24af9f1a25053f7ef0e6a
Nick Garbalau <garbalaunick@protonmail.com>
2022-12-19 18:05:30 +0200

    Add back-end and front-end folders

---
+++ routes/openaiRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const { generateImage } = require('../controllers/openaiController');
+
+router.post('/generateimage', generateImage);
+
+module.exports = router;
